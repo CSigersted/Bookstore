c0bfcf51906958fee6026a7aa7d24d5d81bdbc92
Christian Sigersted <sigersted7@hotmail.com>
2016-04-12 23:10:35 +0200

    implementering af SASS, vireudvikling af Merchello implementering

---
+++ BookstoreUmbraco/App_Code/Models/ProductListDisplay.cs
@@ -0,0 +1,18 @@
+﻿//namespace Models
+//{
+//    using System;
+//    using System.Collections.Generic;
+//    using System.Linq;
+//    using System.Web;
+//    using Merchello.Web.Models.ContentEditing;
+
+////namespace BookstoreUmbraco.App_Code.Models
+////{
+//    public class ProductListDisplay : ProductDisplay
+//    {
+//        /// <summary>
+//        /// Content Id of the ProductDetail page
+//        /// </summary>
+//        public int ProductContentId { get; set; }
+//    }
+//}
